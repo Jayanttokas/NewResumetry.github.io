63a84dc67e98b71add80512edffca06b651fb859
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-05-16 14:38:26 +0530

    fixed issue for image not loading

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ hoverStyle = (i) => {
 
   switch (i) {
     case 1:
-      document.querySelector(".xyz").src = "img/car.png";
+      document.querySelector(".xyz").src = "car.png";
       break;
 
     case 2:
-    document.querySelector(".xyz").src = "img/robo.png";
+    document.querySelector(".xyz").src = "robo.png";
       break;
 
     case 3:
-    document.querySelector(".xyz").src = "img/budget.png";
+    document.querySelector(".xyz").src = "budget.png";
       break;
     default:
 
     case 4:
-    document.querySelector(".xyz").src = "img/piggame.png";
+    document.querySelector(".xyz").src = "piggame.png";
       break;
 
     case 5:
-    document.querySelector(".xyz").src = "img/coffee.png";
+    document.querySelector(".xyz").src = "coffee.png";
     break;
 
     case 6:
-    document.querySelector(".xyz").src = "img/ytsearch.png";
+    document.querySelector(".xyz").src = "ytsearch.png";
     break;
 
     case 7:
-    document.querySelector(".xyz").src = "img/resume.png";
+    document.querySelector(".xyz").src = "resume.png";
     break;
 
     case 8:
-    document.querySelector(".xyz").src = "img/picsapp.png";
+    document.querySelector(".xyz").src = "picsapp.png";
     break;
 
   }
@@ -41,6 +41,6 @@ hoverStyle = (i) => {
 
 removeStyle = (i) => {
   document.querySelector("#project-box" + i).classList.remove("hover");
-  document.querySelector(".xyz").src = "img/third.png";
+  document.querySelector(".xyz").src = "third.png";
 
 }
